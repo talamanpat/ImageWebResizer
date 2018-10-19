fd4e6865244ec1e4101035c7f430941b809c37b5
Talaman <daniel.talaman@gmail.com>
2018-10-19 14:29:01 +0200

    Base ASP Core project + Template + Packeges Magick.Net and SQLite

---
+++ ImageWebResizer/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ImageWebResizer.Models;
+
+namespace ImageWebResizer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Web application for resizing images";
+
+            return View();
+        }
+        
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
